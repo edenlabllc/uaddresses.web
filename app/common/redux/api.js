750b5a3b70f128eaf846636c41ba0ec4e69e6c6a
Denys Orda <dorda@edenlab.com.ua>
2023-05-11 13:31:33 +0300

    fix: add custom headers

--- app/common/redux/api.js
+++ app/common/redux/api.js
@@ -1,4 +1,5 @@
 import { CALL_API } from 'redux-api-middleware';
+import { REACT_APP_CUSTOM_PSK_HEADER } from '../config';
 
 export const invoke = ({ body, headers, ...config }, { auth = true } = {}) => (
   dispatch,
@@ -13,6 +14,7 @@ export const invoke = ({ body, headers, ...config }, { auth = true } = {}) => (
         Pragma: 'no-cache',
         'Cache-Control': 'no-cache',
         Cookie: req ? req.headers.cookie : undefined, // use cookie from request in SSR mode
+        'X-Custom-PSK': REACT_APP_CUSTOM_PSK_HEADER,
         ...headers,
       },
       credentials: auth ? 'same-origin' : 'omit',
