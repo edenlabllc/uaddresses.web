78e052f3442da6fcec2bf2c93b1157ce0ba7cb0b
Denys Orda <dorda@edenlab.com.ua>
2023-07-10 12:36:40 +0300

    fix: remove token verify method

--- app/common/store/index.js
+++ app/common/store/index.js
@@ -1,15 +1,23 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
+import { routerMiddleware, push } from 'react-router-redux';
 import promiseMiddleware from 'redux-promise';
 import thunkMiddleware from 'redux-thunk';
 import multiMiddleware from 'redux-multi';
 import { apiMiddleware } from 'redux-api-middleware';
+import { PUBLIC_INDEX_ROUTE } from 'config';
 
 import rootReducer from '../reducers';
 
+const errorHandlerMiddleware = store => next => (action) => {
+  if (action.error && action.payload.status === 401) {
+    store.dispatch(push(PUBLIC_INDEX_ROUTE));
+  }
+  return next(action);
+};
+
 const middlewares = [
-  multiMiddleware, promiseMiddleware, apiMiddleware,
+  multiMiddleware, promiseMiddleware, apiMiddleware, errorHandlerMiddleware,
 ];
 
 if (process.NODE_ENV !== 'production') {
