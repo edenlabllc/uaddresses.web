c3b2ee5a581f5db1d984bb21a58f701987506491
Denys Orda <dorda@edenlab.com.ua>
2023-07-10 12:36:40 +0300

    fix: remove token verify method

--- app/common/routes/index.js
+++ app/common/routes/index.js
@@ -21,56 +21,32 @@ import StreetUpdatePage from 'containers/pages/StreetUpdatePage';
 import SignInPage from 'containers/pages/SignInPage';
 import NotFoundPage from 'containers/pages/NotFoundPage';
 
-import { verifyToken, getToken } from 'redux/session';
-import { isAuthorized } from 'reducers';
-
-import { PUBLIC_INDEX_ROUTE } from 'config';
-
-export const configureRoutes = ({ store }) => { // eslint-disable-line
-  const requireAuth = async (nextState, replace, next) => {
-    if (__CLIENT__) {
-      if (!isAuthorized(store.getState())) {
-        replace({ pathname: PUBLIC_INDEX_ROUTE });
-      }
-    } else {
-      // FIXME: We should handle case when there is no token passed
-      const token = await store.dispatch(getToken());
-      const { error } = await store.dispatch(verifyToken(token));
-      if (error) {
-        replace({ pathname: PUBLIC_INDEX_ROUTE });
-      }
-    }
-
-    return next();
-  };
-
-  return (
-    <Route component={App}>
-      <Route component={Main} onEnter={requireAuth}>
-        <Route path="/" component={PreloadData}>
-          <IndexRedirect to="regions" />
-          <Route path="regions">
-            <IndexRoute component={RegionsPage} />
-            <Route path=":id" component={RegionUpdatePage} />
-          </Route>
-          <Route path="districts">
-            <IndexRoute component={DistrictsPage} />
-            <Route path=":id" component={DistrictUpdatePage} />
-          </Route>
-          <Route path="settlements">
-            <IndexRoute component={SettlementsPage} />
-            <Route path=":id" component={SettlementUpdatePage} />
-          </Route>
-          <Route path="streets">
-            <IndexRoute component={StreetsPage} />
-            <Route path=":id" component={StreetUpdatePage} />
-          </Route>
+export const configureRoutes = () => (
+  <Route component={App}>
+    <Route component={Main}>
+      <Route path="/" component={PreloadData}>
+        <IndexRedirect to="regions" />
+        <Route path="regions">
+          <IndexRoute component={RegionsPage} />
+          <Route path=":id" component={RegionUpdatePage} />
+        </Route>
+        <Route path="districts">
+          <IndexRoute component={DistrictsPage} />
+          <Route path=":id" component={DistrictUpdatePage} />
+        </Route>
+        <Route path="settlements">
+          <IndexRoute component={SettlementsPage} />
+          <Route path=":id" component={SettlementUpdatePage} />
+        </Route>
+        <Route path="streets">
+          <IndexRoute component={StreetsPage} />
+          <Route path=":id" component={StreetUpdatePage} />
         </Route>
       </Route>
-      <Route path="sign-in" component={SignInPage} />
-      <Route component={Main}>
-        <Route path="*" component={NotFoundPage} />
-      </Route>
     </Route>
-  );
-};
+    <Route path="sign-in" component={SignInPage} />
+    <Route component={Main}>
+      <Route path="*" component={NotFoundPage} />
+    </Route>
+  </Route>
+);
